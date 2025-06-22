3249f929e0a428628739e206c1c9bc206945b258
Dimuthnilanjana <dimuthnilanjana7@gmail.com>
2025-06-22 11:40:27 +0530

    files

---
+++ src/main/java/com/example/demo/controller/testController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/hello")
+    public String helloPath() {
+        return "Hello from Under World!";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetName(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
